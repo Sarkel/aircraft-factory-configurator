466296691602b372f05d45a3d2d0b65a5397d2ad
Sebastian Kubalski <sebastian.kubalski@gmail.com>
2020-01-29 23:08:48 +0100

    init

---
+++ src/main/java/pl/edu/agh/to1/aircraftfactoryconfigurator/AircraftFactoryConfiguratorApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.to1.aircraftfactoryconfigurator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AircraftFactoryConfiguratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AircraftFactoryConfiguratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/agh/to1/aircraftfactoryconfigurator/AircraftFactoryConfiguratorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.to1.aircraftfactoryconfigurator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AircraftFactoryConfiguratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
