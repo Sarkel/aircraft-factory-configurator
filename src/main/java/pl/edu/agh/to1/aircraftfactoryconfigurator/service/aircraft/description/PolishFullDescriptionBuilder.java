ef367a02fdf3910b44c79b4eee28180742612b01
Sebastian Kubalski <sebastian.kubalski@gmail.com>
2020-01-30 21:01:09 +0100

    created polish full description builder

--- src/main/java/pl/edu/agh/to1/aircraftfactoryconfigurator/service/aircraft/description/PolishFullDescriptionBuilder.java
+++ src/main/java/pl/edu/agh/to1/aircraftfactoryconfigurator/service/aircraft/description/PolishFullDescriptionBuilder.java
@@ -2,10 +2,45 @@ package pl.edu.agh.to1.aircraftfactoryconfigurator.service.aircraft.description;
 
 import org.springframework.stereotype.Service;
 
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 public class PolishFullDescriptionBuilder implements FullDescriptionBuilder {
     @Override
     public String build(FullDescriptionDetails descriptionDetails) {
-        return null;
+        StringBuilder descriptionBuilder = new StringBuilder();
+
+        descriptionBuilder.append(descriptionDetails.getModelName());
+        descriptionBuilder.append(" z układem foteli ");
+        descriptionBuilder.append(descriptionDetails.getEconomicSeats());
+        descriptionBuilder.append("-");
+        descriptionBuilder.append(descriptionDetails.getBusinessSeats());
+        descriptionBuilder.append(" (klasa ekonomiczna - biznesowa), z ");
+        descriptionBuilder.append(getFeatureDescription(descriptionDetails.getFeatureDescriptions()));
+        descriptionBuilder.append(". Cena samolotu: ");
+        descriptionBuilder.append(
+                descriptionDetails
+                        .getPrice()
+                        .setScale(2, RoundingMode.HALF_EVEN)
+                        .toString()
+        );
+        descriptionBuilder.append(" zł.");
+
+        return descriptionBuilder.toString();
+    }
+
+    private String getFeatureDescription(Set<String> uniqueFeatureDescriptions) {
+        final List<String> featureDescriptions = new ArrayList<>(uniqueFeatureDescriptions);
+
+        return String.join(
+                ", ",
+                featureDescriptions.stream()
+                        .limit(featureDescriptions.size() - 1)
+                        .collect(Collectors.toSet())
+        ) + featureDescriptions.get(featureDescriptions.size() - 1);
     }
 }
